0d800c5a0b8a3aced05b881914838a489ebe7bec
Aliaksandr Kukharenka <aliaksandr_kukharenka@epam.com>
2022-03-16 14:07:27 +0300

    sad up

--- views/site/sad.php
+++ views/site/sad.php
@@ -31,61 +31,61 @@ $fruits = array(
 );
 ?>
 
-<script>
-Date.prototype.addHours = function(h) {
-    this.setTime(this.getTime() + (h * 60 * 60 * 1000));
-    return this;
-}
+    <script>
+        Date.prototype.addHours = function (h) {
+            this.setTime(this.getTime() + (h * 60 * 60 * 1000));
+            return this;
+        }
 
-function calc($id) {
-    let el = document.getElementById('picker');
-    if (el.value != null && el.value !== '') {
-        let totalItems = document.getElementById('total_items').innerText;
-        for (let i = 0; i < totalItems; i++) {
-            var cardFruit = document.getElementById('cardFruit_' + i);
-            let readyTime = document.getElementById('readyTime_' + i).innerText;
-            let result = new Date(el.value);
-            result.addHours(-1 * readyTime)
-            let resTime = document.getElementById('resTime_' + i);
+        function calc($id) {
+            let el = document.getElementById('picker');
+            if (el.value != null && el.value !== '') {
+                let totalItems = document.getElementById('total_items').innerText;
+                for (let i = 0; i < totalItems; i++) {
+                    var cardFruit = document.getElementById('cardFruit_' + i);
+                    let readyTime = document.getElementById('readyTime_' + i).innerText;
+                    let result = new Date(el.value);
+                    result.addHours(-1 * readyTime)
+                    let resTime = document.getElementById('resTime_' + i);
 
-            if (result < new Date) {
-                resTime.innerText = "Уже никак не успеть :(";
-                cardFruit.classList.add('border-danger');
-                cardFruit.classList.add('card-fruit-none');
+                    if (result < new Date) {
+                        resTime.innerText = "Уже никак не успеть :(";
+                        cardFruit.classList.add('border-danger');
+                        cardFruit.classList.add('card-fruit-none');
+                    } else {
+                        var options = {
+                            weekday: 'long',
+                            month: 'long',
+                            day: 'numeric',
+                            hour: 'numeric',
+                            minute: 'numeric'
+                        };
+                        resTime.innerText = result.toLocaleDateString("ru-RU", options);
+                        cardFruit.classList.add('border-success');
+                        cardFruit.style.border = "2px solid";
+                    }
+
+                }
             } else {
-                var options = {
-                    weekday: 'long',
-                    month: 'long',
-                    day: 'numeric',
-                    hour: 'numeric',
-                    minute: 'numeric'
-                };
-                resTime.innerText = result.toLocaleDateString("ru-RU", options);
-                cardFruit.classList.add('border-success');
-                cardFruit.style.border = "2px solid";
+                let totalItems = document.getElementById('total_items').innerText;
+                for (let i = 0; i < totalItems; i++) {
+                    let cardFruit = document.getElementById('cardFruit_' + i);
+                    cardFruit.classList.remove('card-fruit-none');
+                    cardFruit.classList.remove('border-success');
+                    cardFruit.classList.remove('border-danger');
+
+
+                }
             }
-
         }
-    } else {
-        let totalItems = document.getElementById('total_items').innerText;
-        for (let i = 0; i < totalItems; i++) {
-            let cardFruit = document.getElementById('cardFruit_' + i);
-            cardFruit.classList.remove('card-fruit-none');
-            cardFruit.classList.remove('border-success');
-            cardFruit.classList.remove('border-danger');
+    </script>
 
-
-        }
-    }
-}
-</script>
-
-<div class="row">
-    <div class="col-xl-4 col-md-6 col-sm-12">
-        <div align="center" style="margin-top: 20px; width: 100%">
-            <div class="row" style="margin-bottom: 8px">
-                <div class="col-sm-10">
-                    <?=
+    <div class="row">
+        <div class="col-xl-4 col-md-6 col-sm-12">
+            <div align="center" style="margin-top: 20px; width: 100%">
+                <div class="row" style="margin-bottom: 8px">
+                    <div class="col-sm-10">
+                        <?=
                         DateTimePicker::widget([
                             'id' => 'picker',
                             'readonly' => 'true',
@@ -94,75 +94,71 @@ function calc($id) {
                             'pluginOptions' => ['autoclose' => true],
 
                         ]); ?>
-                </div>
-                <?php
+                    </div>
+                    <?php
                     Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']);
                     ?>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<div>
-    <div class="row">
+    <div>
+        <div class="row">
 
 
+            <?php
+            $i = 0;
+            foreach ($fruits as $item) {
+                $arr = explode("&", $item);
+
+                //ready time
+                $objDateTime = new DateTime('NOW');
+                $objDateTime->modify('+' . $arr[2] . ' hours');
+                $ready_time = $objDateTime->format('D d h:i');
+
+                //trash time
+                $objDateTimeTrash = new DateTime('NOW');
+                $objDateTimeTrash->modify('+' . ($arr[2] + $arr[3]) . ' hours');
+                $trash_time = $objDateTimeTrash->format('D d h:i');
 
 
-        <?php
-                $i = 0;
-                foreach ($fruits as $item) {
-                    $arr = explode("&", $item);
+                echo "<div class=\"col-xl-4 col-md-6 col-sm-12 mb-3\">";
+                echo "<div class=\"card card-fruit\" id=\"cardFruit_$i\">";
+                echo "<div class=\"card-header\">";
+                echo "<h5 class=\"card-title\">$arr[0]</h5>";
+                echo "</div>";
+                echo "<div class=\"card-body\">";
+                echo "<ul class=\"list-group\">";
+                echo "<li class=\"list-group-item\">Бонус: $arr[1]</li>";
+                echo "<li class=\"list-group-item\">Время до роста/порчи: $arr[2] ч / $arr[3] ч";
+                echo "</li>";
+                echo "<li class=\"list-group-item\" style='font-size: 14px'>Посадим сейчас вырастет/испортится:<br>";
+                echo weekDaysReplace($ready_time) . " / " . weekDaysReplace($trash_time);
+                echo "</li>";
+                echo "<li class=\"list-group-item\">Чтобы выросло в нужное время:<br>";
+                echo "<p id='resTime_$i'>выберите время вверху страницы</p>";
+                echo "<span style='display: none' id='readyTime_$i'>$arr[2]</span>";
+                echo "</li>";
+                echo "</div>";
+                echo "</div>";
+                echo "</div>";
 
-                    //ready time
-                    $objDateTime = new DateTime('NOW');
-                    $objDateTime->modify('+' . $arr[2] . ' hours');
-                    $ready_time = $objDateTime->format('D d h:i');
+                $i++;
 
-                    //trash time
-                    $objDateTimeTrash = new DateTime('NOW');
-                    $objDateTimeTrash->modify('+' . ($arr[2] + $arr[3]) . ' hours');
-                    $trash_time = $objDateTimeTrash->format('D d h:i');
-                    
-                   
-
-            echo"<div class=\"col-xl-4 col-md-6 col-sm-12 mb-3\">";
-            echo"<div class=\"card card-fruit\" id=\"cardFruit_$i\">";
-            echo"<div class=\"card-header\">";
-            echo"<h5 class=\"card-title\">$arr[0]</h5>";
-            echo"</div>";
-            echo"<div class=\"card-body\">";
-            echo"<ul class=\"list-group\">";
-            echo"<li class=\"list-group-item\">Бонус: $arr[1]</li>";
-            echo"<li class=\"list-group-item\">Время до роста/порчи: $arr[2] ч / $arr[3] ч";
-            echo"</li>";
-            echo"<li class=\"list-group-item\">Посадим сейчас вырастет/испортится:";
-            echo weekDaysReplace($ready_time). " / " . weekDaysReplace($trash_time);
-            echo"</li>";
-            echo"</div>";
-            echo"<div class=\"card-footer\">";
-            echo"<p id='resTime_$i'>выберите время кнопкой выше</p>";
-            echo"<span style='display: none' id='readyTime_$i'>$arr[2]</span>";
-            echo"</div>";
-            echo"</div>";
-            echo"</div>";
-
-            $i++;
-            
-        }
-            echo" <span style='display: none' id='total_items'>$i</span>";
-        ?>
+            }
+            echo " <span style='display: none' id='total_items'>$i</span>";
+            ?>
 
 
+        </div>
 
-    </div>
 
-
-    <?php
+<?php
 
 function weekDaysReplace($input)
 {
-    $ru_weekdays = array('Пн', 'Вт', 'Ср', 'Чn', 'Пт', 'Ср', 'Вc');
-    $en_weekdays = array('Mon', 'Tue', 'Wed', 'Thursday', 'Fri', 'Sat', 'Sun');
+    $ru_weekdays = array('Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Ср', 'Вc');
+    $en_weekdays = array('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun');
     return str_replace($en_weekdays, $ru_weekdays, $input);
 }
 
