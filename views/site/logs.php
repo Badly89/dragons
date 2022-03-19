74ba3030562dbb2404edaa8b116ee20e50fe18c5
Badly89 <malykinal@mail.ru>
2022-03-19 21:26:56 +0500

    fix logs

--- views/site/logs.php
+++ views/site/logs.php
@@ -8,45 +8,23 @@ $this->params['breadcrumbs'][] = ['label' => 'Админка', 'url' => ['supera
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <style>
-    .row {
-        border: 1px solid black;
-        height: 25px;
-    }
-    
-    .bold {
-        font-weight: bold;
-    }
-    
-    td {
-        /*border: 1px solid black;*/
-        padding: 2px;
-    }
+.row {
+    border: 1px solid black;
+    /* height: 25px; */
+}
 
+.bold {
+    font-weight: bold;
+}
 
+td {
+    /*border: 1px solid black;*/
+    padding: 2px;
+}
 </style>
-<table style="width: 100%">
-    <tr class="row" style="background-color: #666666; color: white">
-        <td class="bold">
-            USER_ID
-        </td>        
-        <td class="bold">
-            USERNAME
-        </td>                
-        <td class="bold">
-            IP
-        </td>               
-        <td class="bold">
-            ACTION
-        </td>         
-        <td class="bold">
-            DATE/TIME
-        </td>                 
-        <td class="bold">
-            RESULT
-        </td>                
-    </tr>
-    <?php
-
+<div class="card">
+    <div class="card-header">
+        <?php 
     if (Users::findGroupById(Yii::$app->user->getId()) == 99) {
         echo Html::beginForm(['/site/logs'], 'post');
         echo Html::hiddenInput('Logs[action]', 'clearLogs');
@@ -54,42 +32,79 @@ $this->params['breadcrumbs'][] = $this->title;
             'Удалить логи', ['class' => 'btn btn-primary', 'onclick' => 'return confirm("Удаляем? ЭТО ОТМЕНИТЬ НЕЛЬЗЯ")']
         );
         echo Html::endForm();
-        echo "<br><hr>";
-    }
+        // echo "<br><hr>";
+    }?>
+    </div>
+    <div class="card-body">
+        <table class="table table-sm table-logs">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">
+                        USER_ID
+                    </th>
+                    <th scope="col">
+                        USERNAME
+                    </th>
+                    <th scope="col">
+                        IP
+                    </th>
+                    <th scope="col">
+                        ACTION
+                    </th>
+                    <th scope="col">
+                        DATE/TIME
+                    </th>
+                    <th scope="col">
+                        RESULT
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
 
+    // echo"";
     foreach ($model->logs as $log) {
         $backColor = "#FFF";
+        $classTr = "table-light";
         if ($log->result == "FAIL" || $log->result == "REJECTED") {
             $backColor = "#ffcac9";
+            $classTr = "table-danger";
+            
         }
         if ($log->result == "SUCCESS" || $log->result == "APPROVED") {
             $backColor = "#cdffc9";
+            $classTr = "table-success";
         }
         ?>
-        <tr style="background-color: <?= $backColor ?>" class="row">
-            <td>
-                <?= $log->user_id ?>
-            </td>
-            <td>
-                <?= $log->username ?>
-            </td>
-            <td>
-                <?= $log->ip ?>
-            </td>
-            <td>
-                <?= $log->action ?>
-            </td>
-            <td>
-                <?= $log->date ?>
-            </td>
-            <td>
-                <?= $log->result ?>
-            </td>
 
 
+                <tr class="<?= $classTr ?>">
 
-        </tr>
-        <?php
+                    <th scope="row">
+                        <?= $log->user_id ?>
+                    </th>
+                    <td>
+                        <?= $log->username ?>
+                    </td>
+                    <td>
+                        <?= $log->ip ?>
+                    </td>
+                    <td>
+                        <?= $log->action ?>
+                    </td>
+                    <td>
+                        <?= $log->date ?>
+                    </td>
+                    <td>
+                        <?= $log->result ?>
+                    </td>
+                </tr>
+
+                <?php
     }
+    // echo" </tbody>";
     ?>
-</table>
+            </tbody>
+        </table>
+    </div>
+</div>
